26ab903bf9b8772ae1deeeca06e82a7a1de6c59d
lemayian23 <lemayianledavit2018@gmail.com>
2026-04-03 15:15:23 +0300

    environment setup

--- app.py
+++ app.py
@@ -2549,5 +2549,10 @@ def create_quotation_post():
     
     return redirect(url_for('quotations'))
 
+import os
+
+# For Render.com compatibility
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
